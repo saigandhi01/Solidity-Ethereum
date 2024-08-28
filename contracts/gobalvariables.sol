5fd1a3a601b26644556da2f5afd015e531485ea7
saigandhi01 <gandhipunem.2001@gmail.com>
2024-08-28 22:33:35 +0530

    Solidity-Ethereum

--- contracts/gobalvariables.sol
+++ contracts/gobalvariables.sol
@@ -1,11 +1,31 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
 
 contract GlobalVariables {
-    function getBlockInfo() public view returns (uint, uint, address) {
-        return (block.number, block.timestamp, block.coinbase);
-    }
     
-    function getTransactionInfo() public view returns (address, uint) {
-        return (msg.sender, msg.value);
+    address public owner;
+    address public Myblockhash;
+    uint256 public difficulty;
+    uint256 public gasLimit;
+    uint256 public number;
+    uint256 public timestamp;
+    uint256 public value;
+    uint256 public nowOn;
+    address public origin;
+    uint256 public gasprice;
+    bytes public callData;
+    bytes4 public firstFour;
+
+
+    constructor() {
+        owner = msg.sender;
+        // here msg.sender is gobal variable, The one who deployed the contract.
+    
+    
+    
+    
+    
     }
+
+
 }
