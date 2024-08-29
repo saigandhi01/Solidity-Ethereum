dc53a7b2f9cb04811d00dc5464aa5dc43e04a41e
saigandhi01 <gandhipunem.2001@gmail.com>
2024-08-29 09:58:39 +0530

    Solidity-Ethereum

--- contracts/Mycoin.sol
+++ contracts/Mycoin.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.16;
 
+
+
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
 
 contract TokenERC20 {
