29959519da64b124ca80edc7fb0ffc7ee3cf5bd7
saigandhi01 <gandhipunem.2001@gmail.com>
2024-09-06 23:09:07 +0530

    Solidity

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,2 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+contract decision {
+    // Declaring state variable 
+    uint public i; 
+    bool public result;
+
+    function Enteravalue(uint _value)public {
+
+        i=_value;
+
+    }
+  
+    // Defining function to  
+    // demonstrate use of  
+    // 'if statement' 
+    function decision_making( 
+    ) public view returns(bool){ 
+        if(i<10){ 
+            return true; 
+            //return result
+        } 
+    } 
+
+    
+}
