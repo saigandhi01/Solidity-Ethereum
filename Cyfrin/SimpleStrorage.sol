dd0b8e8094a88a06e53dcf3242422adc848b5ad4
saigandhi01 <gandhipunem.2001@gmail.com>
2024-10-11 11:07:21 +0530

    Solidity

--- Cyfrin/SimpleStrorage.sol
+++ Cyfrin/SimpleStrorage.sol
@@ -2,5 +2,12 @@
 pragma solidity 0.8.18; // stating our version 
 
 contract SimpleStrorage{
-    
+    /// Basic Types : boolean, uint, int, address, bytes
+
+    bool hasFavoriteNumber = true;
+    uint256 favoriteNumber = 88;
+    int256 favoriteInt = -88;
+    string favoriteNumberText = "nine-nine";
+    address myAddress =  0xB67551da756f059020017Ed058fe1f1d1535D260;
+    bytes32 favoriteBytes32 = "cat";
 }
