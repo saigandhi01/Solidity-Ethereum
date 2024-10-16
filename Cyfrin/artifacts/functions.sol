04518e93ef12169d6a039c227175da4eed14c4ab
saigandhi01 <gandhipunem.2001@gmail.com>
2024-10-17 00:06:48 +0530

    Solidity-functions

--- Cyfrin/artifacts/functions.sol
+++ Cyfrin/artifacts/functions.sol
@@ -3,14 +3,15 @@ pragma solidity ^0.8.0;
 
 contract simpleFunction{
     
-    uint256 favoriteNumber;// 0
+    uint256 public favoriteNumber;// 0
 
     function store(uint256 _favoriteNumber) public  {
           favoriteNumber = _favoriteNumber;
     }
 
-}
-
-    function name(params) {
-        code
+    // view, pure
+    function retrieve() public view returns(uint256){
+         return favoriteNumber;
     }
+
+}
