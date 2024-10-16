c8c66559113d87ece7d46e3e5d66c7655ae009e2
saigandhi01 <gandhipunem.2001@gmail.com>
2024-10-16 23:30:06 +0530

    solidity-functions

---
+++ Cyfrin/artifacts/functions.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract simpleFunction{
+    
+    uint256 favoriteNumber;// 0
+
+    function store(uint256 _favoriteNumber) public  {
+          favoriteNumber = _favoriteNumber;
+    }
+
+}
+
+    function name(params) {
+        code
+    }
