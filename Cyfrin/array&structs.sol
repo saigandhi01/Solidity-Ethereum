533900bda9d438901abe7eba7f7e5cf67dd7a6e6
saigandhi01 <gandhipunem.2001@gmail.com>
2024-10-18 23:41:58 +0530

    Solidity

---
+++ Cyfrin/array&structs.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract simpleFunction{
+    
+    uint256 myFavoriteNumber;// 0
+
+    // uint256[] listOfFavoriteNumbers;
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // Person public pat = Person({favoriteNumber:7, name:"Pat"});
+
+    //dynamic array
+    Person[] public listOfPeople; //[]
+
+    //static array
+    //Person[3] public listOfPeople;
+ 
+    function store(uint256 _favoriteNumber) public  {
+          myFavoriteNumber = _favoriteNumber;
+    }
+
+    // view, pure
+    function retrieve() public view returns(uint256){
+         return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+    }
+
+}
